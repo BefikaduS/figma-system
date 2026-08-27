f7e6838159ea2bba6ba3e484a740159af64aab96
BefikaduS <befikadusisay@gmail.com>
2026-08-27 10:49:34 +0300

    feat: create Figma System application shell

--- code.ts
+++ code.ts
@@ -1,37 +1,85 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+// ============================================================
+// FIGMA SYSTEM
+// Main Plugin Runtime
+// ============================================================
 
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+figma.showUI(__html__, {
+  width: 420,
+  height: 680,
+});
 
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+// ------------------------------------------------------------
+// Message Types
+// ------------------------------------------------------------
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-shapes') {
-    // This plugin creates rectangles on the screen.
-    const numberOfRectangles = msg.count;
-
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < numberOfRectangles; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+type PluginMessage =
+  | {
+      type: "ui-ready";
     }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
+  | {
+      type: "close-plugin";
+    }
+  | {
+      type: "get-selection";
+    };
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+// ------------------------------------------------------------
+// Selection Information
+// ------------------------------------------------------------
+
+function getSelectionInfo() {
+  const selection = figma.currentPage.selection;
+
+  return {
+    count: selection.length,
+    nodes: selection.map((node) => ({
+      id: node.id,
+      name: node.name,
+      type: node.type,
+    })),
+  };
+}
+
+// ------------------------------------------------------------
+// Send message to UI
+// ------------------------------------------------------------
+
+function sendToUI(message: unknown): void {
+  figma.ui.postMessage(message);
+}
+
+// ------------------------------------------------------------
+// Message Handler
+// ------------------------------------------------------------
+
+figma.ui.onmessage = (msg: PluginMessage) => {
+  switch (msg.type) {
+    case "ui-ready": {
+      sendToUI({
+        type: "plugin-ready",
+        selection: getSelectionInfo(),
+      });
+
+      break;
+    }
+
+    case "get-selection": {
+      sendToUI({
+        type: "selection-info",
+        selection: getSelectionInfo(),
+      });
+
+      break;
+    }
+
+    case "close-plugin": {
+      figma.closePlugin();
+      break;
+    }
+
+    default: {
+      const exhaustiveCheck: never = msg;
+      return exhaustiveCheck;
+    }
+  }
 };
